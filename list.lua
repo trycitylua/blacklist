b8a8253c6b546991d7ec14e88a048a995984406c
trycitylua <trycityyy@icloud.com>
2024-11-20 14:24:19 +0100

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,1 +1,31 @@
---
+return {
+    {
+        name = "sanji.design",
+        ids = {
+            "hwid:123456789abcdef",
+            "discord:1215767538768810059",
+            "steam:110000112345678",
+            "license:abcdef1234567890"
+        }
+    },
+
+    {
+        name = "Lisa Musterfrau",
+        ids = {
+            "hwid:abcdef987654321",
+            "discord:123456789012345678",
+            "steam:110000112987654",
+            "license:fedcba0987654321"
+        }
+    },
+
+    {
+        name = "John Doe",
+        ids = {
+            "hwid:987654321abcdef",
+            "discord:543210987654321",
+            "steam:110000113456789",
+            "license:123456abcdef9876"
+        }
+    }
+}
